28468f0c19e07a27782d1e71583dbf4a4ce27b56
Dirk Uys <dirkcuys@gmail.com>
2026-08-06 14:46:58 +0200

    Add filtering by author for blog page

---
+++ assets/js/blog.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', e => {
+
+  const onFilter = (e) => {
+    let author = e.target.value;
+    const toShow = document.querySelectorAll(`article:has(span[data-author="${author}"])`);
+    for (const el of toShow){
+      el.classList.remove('hidden');
+    }
+    const toHide = document.querySelectorAll(`article:not(:has(span[data-author="${author}"]))`);
+    for (const el of toHide){
+      el.classList.add('hidden');
+    }
+  }
+  const select = document.getElementById('author-select')
+  select.addEventListener('change', onFilter);
+
+})
