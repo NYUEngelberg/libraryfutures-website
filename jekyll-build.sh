a1f3b0055c3b9ce49aafbc03df3ddcec1c7c9f71
Dirk Uys <dirkcuys@gmail.com>
2026-09-10 11:55:33 +0200

    Use correct env variable

--- jekyll-build.sh
+++ jekyll-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-bundle exec jekyll contentful --config $JEKYLL_CONFIG_FILE && bundle exec jekyll build --config $JEKYLL_CONFIG_FILE 
+bundle exec jekyll contentful --config $JEKYLL_CONFIG_FILENAME && bundle exec jekyll build --config $JEKYLL_CONFIG_FILENAME
 
