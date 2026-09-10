1849051e9b8f08dcc6372a61d9e15d19d6f78ed4
Dirk Uys <dirkcuys@gmail.com>
2026-09-10 11:47:23 +0200

    Fix --config option

--- jekyll-build.sh
+++ jekyll-build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bundle exec jekyll -config $JEKYLL_CONFIG_FILE contentful && bundle exec jekyll -config $JEKYLL_CONFIG_FILE build --trace
+bundle exec jekyll --config $JEKYLL_CONFIG_FILE contentful && bundle exec jekyll --config $JEKYLL_CONFIG_FILE build --trace
